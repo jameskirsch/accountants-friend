592b1c168ef4ba720a42d7564f595d89f2c4a832
jameskirsch <jjamesdk@yahoo.com>
2018-02-14 13:51:18 -0500

    made some minor changes

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ var liabilities = 5000;
 var assets = liabilities + stockhodersEquity;
 var date = new Date();
 console.log(date);
-if(assets === (stockhodersEquity + liabilities)) {
+
+if (assets !== (stockhodersEquity + liabilities)) {
     console.log('You are in balance');
-}
-else {
+} else if (assets === 1) {
     console.log('you are at risk');
+} else {
+    console.log('nothing');
+}
+
+function myFunc() {
+    return 20;
 }
 
 

330f834657b4c2f77398f28772a53288c9594acc
jameskirsch <jjamesdk@yahoo.com>
2018-02-14 01:13:17 -0500

    created terms and descriptions for accounting, added styles to the page

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('test');
+
+// The Basic Accounting Equation
+var stockhodersEquity = 5000;
+var liabilities = 5000;
+var assets = liabilities + stockhodersEquity;
+
+if(assets === (stockhodersEquity + liabilities)) {
+    console.log('You are in balance');
+}
+else {
+    console.log('you are at risk');
+}
+
+
